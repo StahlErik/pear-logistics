0c1dd9fc79fbb5aa16e4a7414295d110fe8fe641
Erik Stahl <e.stahl95@gmail.com>
2019-02-18 22:20:25 +0100

    Added arrow to show wheter product is incoming or outgoing

--- src/app/pages/shipping-page/shipping-page.component.ts
+++ src/app/pages/shipping-page/shipping-page.component.ts
@@ -14,15 +14,70 @@ export class ShippingPageComponent implements OnInit {
   constructor(private shippingService: ShippingService) {}
 
   addSortedShippings(shippings) {
-    shippings.sort(function(a, b) {
-      if (b.date && a.date) {
-        return b.date - a.date;
+    /* 
+    shippings.sort(function(a, b) { */
+    /*  if (a.date !== null || b.date !== null) {
+        console.log('a: ' + a.date + 'b: ' + b.date);
+        if (a.date > b.date) {
+          console.log('test' + a.date.toDate() + b.date.toDate());
+        }
+      } */
+    /* 
+      if (a.date === null) {
+        return -1;
+      } else if (b.date === null) {
+        return -1;
+      } else if (a.date === b.date) {
+        console.log('what');
+        return 0;
       } else {
-        return b.units - a.units;
+        console.log('test');
+        return a.date.toDate() < b.date.toDate() ? -1 : 1;
+      }
+    }); */
+    /* 
+    let shippingsWithoutNull: Shipping[];
+    let shippingsWithNull: Shipping[];
+    shippings.forEach(element => {
+      if (element.date === null) {
+        shippingsWithNull.push(element);
+      } else {
+        shippingsWithoutNull.push(element);
       }
     });
+    let totalShippings: Shipping[];
+    //totalShippings = shippingsWithNull + shippingsWithoutNull;
+    console.log(shippingsWithNull); */
+    //this.shippings = shippingsWithNull.push(shippingsWithoutNull);
     this.shippings = shippings;
   }
+
+  /*   addSortedShippings(arr, ascending) {
+    // default to ascending
+    if (typeof ascending === 'undefined') ascending = true;
+
+    var multi = ascending ? 1 : -1;
+
+    var sorter = function(a, b) {
+      if (a.date === b.date)
+        // identical? return 0
+        return 0;
+      else if (a.date === null)
+        // a is null? last
+        return 1;
+      else if (b.date === null)
+        // b is null? last
+        return -1;
+      // compare, negate if descending
+      else return a.date.localCompare(b.date) * multi;
+    };
+
+    return arr.sort(sorter);
+  } */
+  /* 
+  addSortedShippings(shippings) {
+    this.shippings = shippings.date.sort();
+  } */
 
   ngOnInit() {
     this.shippingService.getShippings().subscribe(shippings => {
